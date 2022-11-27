a7f9a24bf8d648599f2e53546d4afbdf0fb6b038
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-11-27 13:38:29 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import {useState} from "react";
 
 import './App.css';
 
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
+    const [current, setCurrent] = useState(0);
+
+    const increment = () => {
+       setCurrent(current + 1);
+    }
+
+    const decrement = () => {
+        setCurrent(current - 1);
+    }
+
+    const reset = () => {
+      setCurrent(0);
+    }
+
+
+    return (
+        <>
+            <div>
+                <div>{current}</div>
+                <button onClick={increment}>increment</button>
+                <button onClick={decrement}>decrement</button>
+                <button onClick={reset}>reset</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
